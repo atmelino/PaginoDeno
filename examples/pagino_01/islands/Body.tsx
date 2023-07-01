202f5c1158327d8f6ae73aa4d749c3d80810746d
atmelino <2985251+atmelino@users.noreply.github.com>
2023-06-30 19:55:40 -0500

    readme

--- examples/pagino_01/islands/Body.tsx
+++ examples/pagino_01/islands/Body.tsx
@@ -1,18 +1,27 @@
 import { useEffect, useRef, useState } from "preact/hooks";
-//import { Pagino } from "../../../../PaginoDeno/mod.ts";
+
+/* Development Versions */
+// Import from local storage
 // import { Pagino } from "../../../mod.ts";
-import { Pagino } from "https://raw.githubusercontent.com/atmelino/PaginoDeno/main/mod.ts"
+// Import from github
+// import { Pagino } from "https://raw.githubusercontent.com/atmelino/PaginoDeno/main/mod.ts"
+
+/* Production Versions */
+// Import latest version from deno.land
+import { Pagino } from "https://deno.land/x/paginodeno/mod.ts";
+// Import specific version from deno.land
+// import { Pagino } from "https://deno.land/x/paginodeno@v0.0.1-alpha/mod.ts";
 
 export default function Body() {
   const [currentPage, setcurrentPage] = useState(1);
 
-	function onChange(page: number) {
-		setcurrentPage(page);
-	}
+  function onChange(page: number) {
+    setcurrentPage(page);
+  }
 
   return (
     <>
-      <div class="flex flex-col justify-center w-full pt-1">
+      <div class="m-4 p-2 w-full flex-auto">
         <Pagino
           count={10}
           showFirst={true}
@@ -25,7 +34,7 @@ export default function Body() {
           onChange={onChange}
         />
       </div>
-      <div class=" font-bold text-xl">
+      <div class="m-4 p-2 w-full flex-auto font-bold text-xl">
         <h1>Selected Page: {currentPage}</h1>
       </div>
     </>
